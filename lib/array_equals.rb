f12beaaba56eb309cb81176ab2810a0f8e962ead
ranuseh <rosalyn.anuseh@gmail.com>
2019-03-02 07:15:03 -0800

    Assignment Completed

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,24 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
+
+array1 = [1,2,3,4,5]
+array2 = [1,2,3,4,5]
+
+# # same elements
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1.nil? == array2.nil?
+    if array1.length == array2.length 
+      array1.length.times do |index|
+        if array1[index] != array2[index]
+          return false
+        end
+      end
+      return true
+    end
+    return false
+  end
+  return true
 end
+
+puts array_equals(array1,array2)
+
